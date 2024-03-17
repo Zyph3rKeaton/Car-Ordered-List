96c580da705c485d2872d223c19ea48071f39bf8
Zyph3rKeaton <92408741+Zyph3rKeaton@users.noreply.github.com>
2024-03-16 21:04:33 -0400

    Add files via upload

--- src/Prog01_aOrderedList.java
+++ src/Prog01_aOrderedList.java
@@ -1,10 +1,19 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.Scanner;
-import java.util.Arrays; // Import Arrays utility class
-import java.util.NoSuchElementException;
-
+// Necessary imports for file I/O, array management, and handling specific exceptions.
+import java.io.File;                      // Used for file operations, to represent files and directories in the file system.
+import java.io.FileNotFoundException;     // Exception thrown when attempting to read from a file that does not exist.
+import java.io.PrintWriter;               // Enables writing formatted data to an output stream, such as a file.
+import java.util.Scanner;                 // Facilitates reading input from various sources, including files.
+import java.util.Arrays;                  // Provides static methods to manipulate arrays, such as sorting and searching.
+import java.util.NoSuchElementException;  // Thrown by various accessor methods to indicate the element being requested does not exist.
+/**
+ * Main class for the Car OrderedList Manager. Handles file input/output and processes lines to perform add or delete operations on a list of cars.
+ *
+ * CSC 1351 Programming Project No 1
+ * Section 05
+ *
+ * @author Keaton Hodgson
+ * @since 03/17/2024
+ */
 public class Prog01_aOrderedList {
     public static void main(String[] args) {
         try {
@@ -24,7 +33,18 @@ public class Prog01_aOrderedList {
             System.out.println("Program execution cancelled or file not found.");
         }
     }
-
+    /**
+     * Processes a line from the input file to either add a new Car to the list or delete one based on its make and year.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 05
+     *
+     * @author Keaton Hodgson
+     * @since 03/17/2024
+     *
+     * @param orderedList The ordered list of cars.
+     * @param line The line read from the input file.
+     */
     private static void processLine(aOrderedList<Car> orderedList, String line) {
         String[] tokens = line.split(",");
         if ("A".equalsIgnoreCase(tokens[0])) {
@@ -43,7 +63,18 @@ public class Prog01_aOrderedList {
             }
         }
     }
-
+    /**
+     * Outputs the final list of cars to a specified PrintWriter in a custom format.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 05
+     *
+     * @author Keaton Hodgson
+     * @since 03/17/2024
+     *
+     * @param output The PrintWriter to write output to.
+     * @param orderedList The ordered list of cars.
+     */
     private static void outputCustomFormat(PrintWriter output, aOrderedList<Car> orderedList) {
         output.println("Number of cars: " + orderedList.size());
         orderedList.reset();
@@ -54,7 +85,20 @@ public class Prog01_aOrderedList {
             output.printf("Price: $%,d\n\n", car.getPrice());
         }
     }
-
+    /**
+     * Prompts the user for the filename of an input file and returns a Scanner object for reading from it.
+     * If the file does not exist, the user is prompted to re-enter a filename or cancel the operation.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author Keaton Hodgson
+     * @since 03/17/2024
+     *
+     * @param userPrompt The prompt to display to the user.
+     * @return Scanner object for reading the input file.
+     * @throws FileNotFoundException If the user cancels the operation or file cannot be found/opened.
+     */
     public static Scanner GetInputFile(String userPrompt) throws FileNotFoundException {
         Scanner userInput = new Scanner(System.in);
         System.out.println(userPrompt);
@@ -72,7 +116,20 @@ public class Prog01_aOrderedList {
             }
         }
     }
-
+    /**
+     * Prompts the user for the filename of an output file and returns a PrintWriter object for writing to it.
+     * If the file cannot be created or opened, the user is prompted to re-enter a filename or cancel the operation.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author keatonhodgson
+     * @since 03/17/2024
+     * 
+     * @param userPrompt The prompt to display to the user.
+     * @return PrintWriter object for writing to the output file.
+     * @throws FileNotFoundException If the user cancels the operation or file cannot be found/opened.
+     */
     public static PrintWriter GetOutputFile(String userPrompt) throws FileNotFoundException {
         Scanner userInput = new Scanner(System.in);
         System.out.println(userPrompt);
@@ -92,58 +149,171 @@ public class Prog01_aOrderedList {
 }
 
 
-// Car class definition
+/**
+ * Represents a car with make, year, and price attributes. Allows for comparison based on make and year.
+ *
+ * CSC 1351 Programming Project No 1
+ * Section 02
+ *
+ * @author Keaton Hodgson
+ * @since 03/17/2024
+ */
 class Car implements Comparable<Car> {
-    private String make;
-    private int year;
-    private int price;
+    // Member variables with in-line comments
+    private String make; // The manufacturer of the car
+    private int year; // The manufacturing year of the car
+    private int price; // The price of the car in USD
 
+    // Constructor
+    /**
+     * Constructs a Car instance with specified make, year, and price.
+     *
+	 * CSC 1351 Programming Project No 1
+	 * Section 02
+	 *
+	 * @author Keaton Hodgson
+	 * @since 03/17/2024
+     *
+     * @param make The make of the car
+     * @param year The year the car was made
+     * @param price The price of the car
+     */
     public Car(String make, int year, int price) {
         this.make = make;
         this.year = year;
         this.price = price;
     }
+    // Getters
 
+    /**
+     * Retrieves the make of the car.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author keatonhodgson
+     * @since 03/17/2024
+     * 
+     * @return The make of the car.
+     */
     public String getMake() {
         return make;
     }
-
+    /**
+     * Retrieves the manufacturing year of the car.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author keatonhodgson
+     * @since 03/17/2024
+     * 
+     * @return The year of the car.
+     */
     public int getYear() {
         return year;
     }
-
+    /**
+     * Retrieves the price of the car.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author keatonhodgson
+     * @since 03/17/2024
+     * 
+     * @return The price of the car in USD.
+     */
     public int getPrice() {
         return price;
     }
 
+    /**
+     * Provides a string representation of the Car object, including its make, year, and price.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author Keaton Hodgson
+     * @since 03/17/2024
+     *
+     * @return A string that represents the Car object.
+     */
+    @Override
     public String toString() {
-        return "Make: " + make + ", Year: " + year + ", Price: " + price + ";";
+        return "Make: " + make + ", Year: " + year + ", Price: $" + price + ";";
     }
+
+    /**
+     * Compares this Car object with another Car object for order. Cars are first compared by their make. 
+     * If the makes are the same, then their years are compared.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author Keaton Hodgson
+     * @since 03/17/2024
+     *
+     * @param other The Car object to be compared.
+     * @return A negative integer, zero, or a positive integer as this Car is less than, equal to, 
+     *         or greater than the specified Car.
+     */
+    @Override
     public int compareTo(Car other) {
         int makeCompare = this.make.compareTo(other.make);
         if (makeCompare != 0) return makeCompare;
         return Integer.compare(this.year, other.year);
     }
 }
-
-// aOrderedList class definition updated for generic Comparable type
+/**
+ * A generic ordered list class for managing a dynamic array of Comparable objects.
+ * This class supports adding and removing elements while maintaining the order of elements.
+ * It also provides iterator methods for traversing through the list.
+ *
+ * CSC 1351 Programming Project No 1
+ * Section 02
+ *
+ * @author Keaton Hodgson
+ * @since 03/17/2024
+ */
 class aOrderedList<T extends Comparable<T>> {
-    final int SIZE_INCREMENTS = 20;
-    private T[] oList;
-    private int numObjects;
-    private int curr; // Iterator current position
+    final int SIZE_INCREMENTS = 20; // Defines the size increments for the array expansion.
+    private T[] oList; // The array storing the ordered list of Comparable objects.
+    private int numObjects; // Tracks the number of objects in the ordered list.
+    private int curr; // Iterator index, indicating the current position for iteration.
 
+    /**
+     * Constructs an empty ordered list with an initial capacity.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author keatonhodgson
+     * @since 03/17/2024
+     * 
+     */
     @SuppressWarnings("unchecked")
     public aOrderedList() {
         this.oList = (T[]) new Comparable[SIZE_INCREMENTS];
         this.numObjects = 0;
     }
-
+    /**
+     * Adds a new object to the ordered list in its correct position to maintain order.
+     * 
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author keatonhodgson
+     * @since 03/17/2024
+     * 
+     * @param newObject The new Comparable object to be added.
+     */
     public void add(T newObject) {
         if (numObjects == oList.length) {
             oList = Arrays.copyOf(oList, oList.length + SIZE_INCREMENTS);
         }
-        int insertIndex = 0;
+        int insertIndex = 0; // Index to insert the new object at
+
         while (insertIndex < numObjects && oList[insertIndex].compareTo(newObject) < 0) {
             insertIndex++;
         }
@@ -151,7 +321,18 @@ class aOrderedList<T extends Comparable<T>> {
         oList[insertIndex] = newObject;
         numObjects++;
     }
-    // Returns the element at the specified position in this list
+    /**
+     * Returns the element at the specified position in this list
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author keatonhodgson
+     * @since 03/17/2024
+     * 
+     * @param index
+     * @return
+     */
     public T get(int index) {
         if (index >= 0 && index < numObjects) {
             return oList[index];
@@ -159,30 +340,102 @@ class aOrderedList<T extends Comparable<T>> {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + numObjects);
         }
     }
-    // Returns the number of elements in this list
+    /**
+     *  Returns the number of elements in this list
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author keatonhodgson
+     * @since 03/17/2024
+     * 
+     * @return
+     */
     public int size() {
         return numObjects;
     }
-    // Returns true if the array is empty and false otherwise
+    /**
+     * Checks if the list is empty.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author keatonhodgson
+     * @since 03/17/2024
+     * 
+     * @return boolean True if the list is empty, false otherwise.
+     */
     public boolean isEmpty() {
         return numObjects == 0;
     }
-    public boolean hasNext() { return curr < numObjects; }
+
+    /**
+     * Determines if there are more elements in the list to iterate over.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author keatonhodgson
+     * @since 03/17/2024
+     * 
+     * @return boolean True if there is at least one more element, false otherwise.
+     */
+    public boolean hasNext() {
+        return curr < numObjects;
+    }
     
+    /**
+     * Returns the next element in the iteration and increments the iterator index.
+     * Throws NoSuchElementException if the iteration has no more elements.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author keatonhodgson
+     * @since 03/17/2024
+     * 
+     * @return T The next element in the list.
+     * @throws NoSuchElementException If there are no more elements to iterate.
+     */
     public T next() {
         if (!hasNext()) throw new NoSuchElementException("No more elements.");
         return oList[curr++];
     }
 
-    // Removes the element at the specified position in this list
+    /**
+     * Removes the element at the specified index in the list. Adjusts the array accordingly to fill the gap.
+     * Throws IndexOutOfBoundsException if the index is out of bounds.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author keatonhodgson
+     * @since 03/17/2024
+     * 
+     * @param index The index of the element to remove.
+     * @throws IndexOutOfBoundsException If index is outside the list bounds.
+     */
     public void remove(int index) {
         if (index < 0 || index >= numObjects) {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + numObjects);
         }
         System.arraycopy(oList, index + 1, oList, index, numObjects - index - 1);
         numObjects--;
-        oList[numObjects] = null; // Help GC
+        oList[numObjects] = null; // Help with garbage collection by nulling the last element.
     }
+
+    /**
+     * Removes the last element returned by the next() method from the list.
+     * Throws IllegalStateException if this method is called without calling next() first.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author keatonhodgson
+     * @since 03/17/2024
+     * 
+     * @throws IllegalStateException If next() was not called before this remove method.
+     */
     public void removeLastIterated() {
         if (curr <= 0) {
             throw new IllegalStateException("removeLastIterated() called without a next() call");
@@ -190,7 +443,17 @@ class aOrderedList<T extends Comparable<T>> {
         this.remove(--curr);
     }
 
-    // toString method to return the list contents in the specified format
+    /**
+     * Provides a string representation of the list, formatting the elements enclosed in brackets and separated by commas.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author keatonhodgson
+     * @since 03/17/2024
+     * 
+     * @return String A string representation of the list elements.
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("[");
@@ -203,8 +466,20 @@ class aOrderedList<T extends Comparable<T>> {
         sb.append("]");
         return sb.toString();
     }
-    public void reset() { curr = 0; }
 
+    /**
+     * Resets the iterator to the start of the list, so the next call to next() will return the first element.
+     *
+     * CSC 1351 Programming Project No 1
+     * Section 02
+     *
+     * @author keatonhodgson
+     * @since 03/17/2024
+     */
+    public void reset() {
+        curr = 0;
+    }
 }
+
 
 
